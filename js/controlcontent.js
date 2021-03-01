d837fadcc10bb6340cd65697ae0b55b08f15798a
Dima Trofimchuk <dimatrofimchuk@ukr.net>
2021-03-01 18:21:27 +0200

    start to do lab4

--- js/controlcontent.js
+++ js/controlcontent.js
@@ -1,11 +1,13 @@
 let btns = [];
 let content = [];
+let count;
 document.body.onload = function(){
-    
+    count = document.querySelectorAll("#left-btns li").length;
     // btns.push(document.getElementById('btn_1'));
-    // alert( btns );
+    // alert( count );
     // element = document.getElementById(id);
-    for (let i = 0; i < 9; i++) {
+    
+    for (let i = 0; i < count; i++) {
         // alert( 'btn_'+ i );
         btns.push(document.getElementById('btn_'+i));
         content.push(document.getElementById('content_'+i));
@@ -16,7 +18,7 @@ document.body.onload = function(){
 
 function set_content(id){
 
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < count; i++) {
         // alert( 'btn_'+ i );
         btns[i].classList.remove("active-btn");
         content[i].classList.remove("active-content");
